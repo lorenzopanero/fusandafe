5d70a9779868dfaa7c7196ed28957c5e694e5813
Enzimush <enzimush@gmail.com>
2025-05-06 21:18:52 +0200

    Add Podfile and Podfile.lock; implement pod helper for Flutter plugins

---
+++ ios/Flutter/podhelper.rb
@@ -0,0 +1,24 @@
+# Install pods needed for Flutter plugins.
+def install_all_flutter_pods(flutter_application_path = nil)
+    flutter_application_path ||= File.join(__dir__, '..', '..')
+    load File.join(flutter_application_path, '.flutter-plugins-dependencies')
+  
+    plugin_pods = parse_KV_file(File.join(flutter_application_path, '.flutter-plugins'))
+    plugin_pods.each do |name, path|
+      symlink = File.join('.symlinks', 'plugins', name)
+      FileUtils.mkdir_p(symlink)
+      FileUtils.ln_sf(path, symlink)
+      pod name, :path => File.join(symlink, 'ios')
+    end
+  end
+  
+  # Parse a key-value file.
+  def parse_KV_file(file, separator='=')
+    map = {}
+    File.foreach(file) do |line|
+      next if line.strip.empty? || line.start_with?('#')
+      key, value = line.strip.split(separator, 2)
+      map[key] = value
+    end
+    map
+  end
